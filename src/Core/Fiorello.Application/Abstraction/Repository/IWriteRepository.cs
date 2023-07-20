6b5ab308d7cc21c7ab7903b9314da616beff7684
shemshajili <shamsah@code.edu.az>
2023-07-20 07:50:03 +0400

    BE-005 IWriteRepository Created.

---
+++ src/Core/Fiorello.Application/Abstraction/Repository/IWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using Fiorello.Domain.Entities.Common;
+
+namespace Fiorello.Application.Abstraction.Repository;
+
+public interface IWriteRepository<T> : IRepository<T> where T : BaseEntity, new()
+{
+    Task AddAsync(T entity);
+    Task AddRangeAsync(ICollection<T> entites);
+    void Remove(T entity);
+    void RemoveRange(ICollection<T> entites);
+    void Update(T entity);
+    Task SaveChangeAsync();
+}
