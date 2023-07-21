18f8def3628d6e57645c13473a76f537fbe72e52
shemshajili <shamsah@code.edu.az>
2023-07-21 10:09:18 +0400

    BE-007 added AutoMapper

---
+++ src/Infrastructure/Fiorello.Persistance/MapperProfiles/CategoryProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using System.Reflection.Metadata;
+
+namespace Fiorello.Persistance.MapperProfiles;
+
+public class CategoryProfile:Profile
+{
+    public CategoryProfile() 
+    {
+        CreateMap<User,UserViewModel>();
+    }
+
+}
