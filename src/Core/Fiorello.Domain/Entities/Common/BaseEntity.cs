b57ab8758af32e2200f66b53e0ebe1d017513262
Leman <leman@Leman-MacBook-Air.local>
2023-07-21 07:34:35 +0400

    SaveChangesInterceptor is added

--- src/Core/Fiorello.Domain/Entities/Common/BaseEntity.cs
+++ src/Core/Fiorello.Domain/Entities/Common/BaseEntity.cs
@@ -3,7 +3,7 @@
 public abstract class BaseEntity
 {
     public Guid Id { get; set; }
-    public bool IsDeleted { get; set; }
+    public virtual bool IsDeleted { get; set; }
     public DateTime DateCreated { get; set; }
     public DateTime DateModified { get; set; }
 }
