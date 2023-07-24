48ab1cbc3108d9f72828b7da22d78f49bb0cc846
Leman <leman@Leman-MacBook-Air.local>
2023-07-25 02:24:24 +0400

    BE-010 RegisterDto created

---
+++ src/Core/Fiorello.Application/Abstraction/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Fiorello.Application.DTOs.AuthDTOs;
+
+namespace Fiorello.Application.Abstraction.Services;
+
+public interface IAuthService
+{
+    Task Register(RegisterDto registerDto);
+}
+
