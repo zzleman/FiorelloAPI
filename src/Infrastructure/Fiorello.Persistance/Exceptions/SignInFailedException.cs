167d993f22ab7d46e4812377d50b1e578b1adac4
Elmir202 <elmireh@code.edu.az>
2023-07-25 11:42:22 +0400

    SignInFailed Exception added

---
+++ src/Infrastructure/Fiorello.Persistance/Exceptions/SignInFailedException.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace Fiorello.Persistance.Exceptions;
+
+public class SignInFailedException : Exception, IBaseException
+{
+    public int StatusCode { get; set; }
+    public string CustomMessage { get; set; }
+
+    public SignInFailedException(string message) : base(message)
+    {
+        StatusCode=(int)HttpStatusCode.BadRequest;
+        CustomMessage=message;
+    }
+}
