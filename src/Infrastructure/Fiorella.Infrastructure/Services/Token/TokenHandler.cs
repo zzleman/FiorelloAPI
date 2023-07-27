52e806399bab2b8a2a359361ab1063380d8e4301
Leman <leman@Leman-MacBook-Air.local>
2023-07-27 09:47:12 +0400

    BE-014 Infrastructure bug is fixed

--- src/Infrastructure/Fiorella.Infrastructure/Services/Token/TokenHandler.cs
+++ src/Infrastructure/Fiorella.Infrastructure/Services/Token/TokenHandler.cs
@@ -8,7 +8,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace Fiorella.Infrastructure.Services.Token;
+namespace Fiorello.Infrastructure.Services.Token;
 
 public class TokenHandler : ITokenHndler
 {
